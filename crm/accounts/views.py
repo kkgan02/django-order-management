9268116deb41d01877aba803dfaec57ebb145c03
kkgan02 <gankk02@gmail.com>
2024-01-21 14:25:53 +0800

    html -> view : to render html instead of string

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, "accounts/dashboard.html")
 
 
 def products(request):
-    return HttpResponse("Products")
+    return render(request, "accounts/products.html")
 
 
 def customer(request):
-    return HttpResponse("Customer")
+    return render(request, "accounts/customer.html")
