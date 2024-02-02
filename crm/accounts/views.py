8b1de888e05f5e668d3eeb0a62661cba6e2a46d9
kkgan02 <gankk02@gmail.com>
2024-02-02 23:19:25 +0800

    update button function

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -47,3 +47,15 @@ def createOrder(request):
 
     context = {"form": form}
     return render(request, "accounts/order_form.html", context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            return redirect("/")
+    context = {"form": form}
+    return render(request, "accounts/order_form.html", context)
