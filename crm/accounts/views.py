9a5f67981fd05c6614ab6fe2eae50b81a8e0ee3f
kkgan02 <gankk02@gmail.com>
2024-02-12 23:25:05 +0800

    added successful message for registration at login page

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -5,6 +5,7 @@ from .models import *
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def registerPage(request):
@@ -13,6 +14,10 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + user)
+            return redirect("login")
+
     context = {"form": form}
     return render(request, "accounts/register.html", context)
 
