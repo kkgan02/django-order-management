81e8f78894a41d409ce1ef896c77c09181b1fc61
kkgan02 <gankk02@gmail.com>
2024-02-13 23:00:02 +0800

    create decorator to replace conditional logic for unauthenticated_user

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -4,44 +4,42 @@ from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+from .decorators import unauthenticated_user
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
+@unauthenticated_user
 def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect("home")
-    else:
-        form = CreateUserForm()
-        if request.method == "POST":
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get("username")
-                messages.success(request, "Account was created for " + user)
-                return redirect("login")
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + user)
+            return redirect("login")
 
-        context = {"form": form}
-        return render(request, "accounts/register.html", context)
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
 
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect("home")
-    else:
-        if request.method == "POST":
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-            else:
-                messages.info(request, "Username or password is incorrect")
 
-        context = {}
-        return render(request, "accounts/login.html", context)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.info(request, "Username or password is incorrect")
+
+    context = {}
+    return render(request, "accounts/login.html", context)
 
 
 def logoutUser(request):
@@ -68,6 +66,11 @@ def home(request):
         "pending": pending,
     }
     return render(request, "accounts/dashboard.html", context)
+
+
+def userPage(request):
+    context = {}
+    return render(request, "accounts/user.html", context)
 
 
 @login_required(login_url="login")
