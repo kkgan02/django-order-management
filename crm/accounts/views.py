0e5f0df5e38f891c649fbd66ce81e4a232ecbf15
kkgan02 <gankk02@gmail.com>
2024-02-20 00:18:23 +0800

    user setting form

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 from django.contrib.auth import authenticate, login, logout
@@ -97,7 +97,15 @@ def userPage(request):
 @login_required(login_url="login")
 @allowed_users(allowed_roles=["customer"])
 def accountSettings(request):
-    context = {}
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == "POST":
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {"form": form}
     return render(request, "accounts/account_settings.html", context)
 
 
