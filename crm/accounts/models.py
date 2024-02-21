bf8fadaadd02055164958b8b8abe8a13695a62a5
kkgan02 <gankk02@gmail.com>
2024-02-21 23:44:02 +0800

    fix and uncomment previous existed model

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -4,8 +4,59 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    profile_pic = models.ImageField(default="billy1.jpg", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ("Indoor", "Indoor"),
+        ("Outdoor", "Outdoor"),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ("Pending", "Pending"),
+        ("Out for delivery", "Out for delivery"),
+        ("Delivered", "Delivered"),
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    note = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.product.name
+
+
 class Profile(models.Model):
-    user = models.OneToOneRel(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     first_name = models.CharField(max_length=200, null=True, blank=True)
     last_name = models.CharField(max_length=200, null=True, blank=True)
     phone = models.CharField(max_length=200, null=True, blank=True)
@@ -34,54 +85,3 @@ def update_profile(sender, instance, created, **kwargs):
 
 
 # post_save.connect(update_profile, sender=User)
-
-
-# class Customer(models.Model):
-#     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=200, null=True)
-#     phone = models.CharField(max_length=200, null=True)
-#     email = models.CharField(max_length=200, null=True)
-#     profile_pic = models.ImageField(default="billy1.jpg", null=True, blank=True)
-#     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Tag(models.Model):
-#     name = models.CharField(max_length=200, null=True)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Product(models.Model):
-#     CATEGORY = (
-#         ("Indoor", "Indoor"),
-#         ("Outdoor", "Outdoor"),
-#     )
-#     name = models.CharField(max_length=200, null=True)
-#     price = models.FloatField(null=True)
-#     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-#     description = models.CharField(max_length=200, null=True, blank=True)
-#     date_created = models.DateTimeField(auto_now_add=True, null=True)
-#     tags = models.ManyToManyField(Tag)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Order(models.Model):
-#     STATUS = (
-#         ("Pending", "Pending"),
-#         ("Out for delivery", "Out for delivery"),
-#         ("Delivered", "Delivered"),
-#     )
-#     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-#     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-#     date_created = models.DateTimeField(auto_now_add=True, null=True)
-#     status = models.CharField(max_length=200, null=True, choices=STATUS)
-#     note = models.CharField(max_length=100, null=True)
-
-#     def __str__(self):
-#         return self.product.name
