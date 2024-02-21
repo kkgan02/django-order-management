8763f8efccee1715b6fe6cb4821575d2732a033c
kkgan02 <gankk02@gmail.com>
2024-02-21 23:34:17 +0800

    method 2 using decorator to replace receiver signal

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -13,6 +14,7 @@ class Profile(models.Model):
         return str(self.use)
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
 
     if created:
@@ -20,9 +22,10 @@ def create_profile(sender, instance, created, **kwargs):
         print("Profile created")
 
 
-post_save.connect(create_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
 
 
+@receiver(post_save, sender=User)
 def update_profile(sender, instance, created, **kwargs):
 
     if created == False:
@@ -30,7 +33,7 @@ def update_profile(sender, instance, created, **kwargs):
         print("Profile created")
 
 
-post_save.connect(update_profile, sender=User)
+# post_save.connect(update_profile, sender=User)
 
 
 # class Customer(models.Model):
