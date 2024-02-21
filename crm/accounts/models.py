92da3e4f834df5e9d64a41dd6dd726b3cafaf90a
kkgan02 <gankk02@gmail.com>
2024-02-22 00:01:04 +0800

    move signal part to signals.py

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Customer(models.Model):
@@ -63,25 +61,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.use)
-
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-
-    if created:
-        Profile.objects.create(user=instance)
-        print("Profile created")
-
-
-# post_save.connect(create_profile, sender=User)
-
-
-@receiver(post_save, sender=User)
-def update_profile(sender, instance, created, **kwargs):
-
-    if created == False:
-        instance.profile.save()
-        print("Profile created")
-
-
-# post_save.connect(update_profile, sender=User)
